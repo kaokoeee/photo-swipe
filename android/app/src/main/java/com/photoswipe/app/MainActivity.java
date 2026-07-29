f3f7241b9e02da5c46b1042b9e6eb3bab2942ab3
kaokoeee <kaokoeee@gmail.com>
2026-07-29 13:45:19 +0800

    Integrate native GalleryPlugin for real photo pick & delete
    - Add GalleryPlugin.java: native system photo picker + MediaStore delete
    - Update MainActivity to register the plugin
    - Add Android permissions: READ_MEDIA, WRITE_EXTERNAL_STORAGE
    - HTML with Capacitor bridge: native pick on APK, file input on PWA

--- android/app/src/main/java/com/photoswipe/app/MainActivity.java
+++ android/app/src/main/java/com/photoswipe/app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.photoswipe.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.photoswipe.app.plugins.GalleryPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(GalleryPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
